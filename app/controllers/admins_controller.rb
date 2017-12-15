bf686962743b0fb652f382b2c7232305ffdfe914
th30r3tisch <theoretisch@hotmail.de>
2017-12-15 01:55:46 +0100

    optimized styling an fixed some mistakes

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,26 +1,26 @@
 class AdminsController < ApplicationController
   before_action :set_game, only: [:show, :edit, :update, :destroy]
-	
+
 	def index
     	@events = Event.all
   	end
-	
+
 	def show
 	end
-	
+
 	def new
     	@admin = Admin.new
   	end
-	
+
 	def edit
   	end
-	
+
 	private
 	# Use callbacks to share common setup or constraints between actions.
     def set_game
-      	@game = Game.find(params["id"])
+      	@admin = Admin.find(params["id"])
     end
-	
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
       params.require(:admin).permit(:name, :email, :event_id)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def searchPath()
+    games_path if request.path_info.include?('games') else
+    events_path if request.path_info.include?('events')
+  end
 end
