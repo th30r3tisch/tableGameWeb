f4f6404cd5f35527e301503200dc1091ddabf9d3
th30r3tisch <theoretisch@hotmail.de>
2018-01-04 15:11:40 +0100

    fixed warning and added testdb migration to travis.yml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def searchPath()
-    games_path if request.path_info.include?('games') else
+    games_path if request.path_info.include?('games')
     events_path if request.path_info.include?('events')
   end
 end
